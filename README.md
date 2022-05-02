bd066b425bc5bf92de54480967b14d924b0cbaea
PAULA <deyvison.paula@atos.net>
2022-05-02 11:45:33 -0300

    Entity Employees - Paginando Usuarios

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ﻿# Curso NET 6 Web Api - Udemy 
 ### Instrutor: Stephany Henrique de Almeida
-### Repositório do Projeto Professor: [GitGub](https://github.com/StephanyBatista/IWantApp)
+### Repositório do Projeto Professor: [GitGub](https://github.com/StephanyBatista)
 
 ## IWantApp
 _Repository to teach students about .NET 6 and how to build a web app in the real world._
--- src/Endpoints/Employees/EmployeeGetAll.cs
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -8,9 +8,10 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(UserManager<IdentityUser> userManager)
+    //Realizando um GetAll por paginação
+    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
     {
-        var users = userManager.Users.ToList();
+        var users = userManager.Users.Skip((page - 1) * rows).Take(rows).ToList(); //Percorre a cada 'x' linhas
         var employees = new List<EmployeeResponse>();
         foreach (var item in users)
         {
@@ -21,4 +22,18 @@ public class EmployeeGetAll
         }
         return Results.Ok(employees);
     }
+
+    //public static IResult Action(UserManager<IdentityUser> userManager)
+    //{
+    //    var users = userManager.Users.ToList();
+    //    var employees = new List<EmployeeResponse>();
+    //    foreach (var item in users)
+    //    {
+    //        var claims = userManager.GetClaimsAsync(item).Result;
+    //        var claimName = claims.FirstOrDefault(c => c.Type == "Name");
+    //        var userName = claimName != null ? claimName.Value : string.Empty;
+    //        employees.Add(new EmployeeResponse(item.Email, userName));
+    //    }
+    //    return Results.Ok(employees);
+    //}
 }
